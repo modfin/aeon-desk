ff85e86845ad9ed1d6ea768afdb66ff50e418e68
Rasmus Holm <c.r.holm@gmail.com>
2025-11-16 15:12:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-
-
 flatpak uninstall --assumeyes --noninteractive org.mozilla.firefox
-
 flatpak install --assumeyes --noninteractive com.google.Chrome
 flatpak install --assumeyes --noninteractive flathub com.bitwarden.desktop
-
 flatpak install --assumeyes --noninteractive org.onlyoffice.desktopeditors
 flatpak install --assumeyes --noninteractive flathub com.github.IsmaelMartinez.teams_for_linux
-
 flatpak install --assumeyes --noninteractive flathub com.spotify.Client
-
+flatpak install --assumeyes --noninteractive flathub us.zoom.Zoom
 
 ## Installing a Flameshot, and setting up keys
 ## distrobox tumbleweed create flameshot to default (it seems broken in flatpak)
@@ -56,6 +51,22 @@ gsettings set org.gnome.desktop.wm.preferences auto-raise-delay 0
 ## Setting normal scrollingn
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 
+## Setting background
+
+BACKGROUND="$HOME/.local/share/backgrounds/mf.bg.jpg"
+curl https://raw.githubusercontent.com/modfin/aeon-desk/refs/heads/master/bg.jpg > $BACKGROUND
+echo "Setting background to $BACKGROUND"
+gsettings set org.gnome.desktop.background picture-uri $BACKGROUND
+gsettings set org.gnome.desktop.background picture-uri-dark $BACKGROUND
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+
+## Setting windows header
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
+## Setting fonts
+gsettings set org.gnome.desktop.interface font-name 'Open Sans 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Open Sans Bold 11'
+
 ## Tools
 distrobox enter tumbleweed -- sudo zypper --non-interactive install nano git htop btop sensors
-
